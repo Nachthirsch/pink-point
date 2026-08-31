8d5311f798587b94011f2997bab20542b29b90c8
Nachthirsch <revengecounter202@gmail.com>
2026-08-31 09:35:22 +0700

    Sort certificates by date, newest first

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,23 +1,5 @@
-/* Certificates data — sourced from certificates.jsx */
+/* Certificates data — reverse chronological order (newest first) */
 export const certificates = [
-  {
-    name: "IT BootCamp Software Development",
-    issuer: "Fakultas Teknik & Informatik Universitas Bina Sarana Indonesia",
-    date: "June 2023",
-    credential: "https://says.bsi.ac.id/e_sertifikat_lykan-MzE3MTE1MTExNTU=.html",
-  },
-  {
-    name: "Become an AWS Cloud Engineer (Talent Scouting Academy",
-    issuer: "Digital Talent Scholarship",
-    date: "Mar 2024",
-    credential: "https://drive.google.com/file/d/1gzDQfXXCdimIKrbfnDhdhSOuWsB6TjyX/view?usp=drive_link",
-  },
-  {
-    name: "Program Analyst",
-    issuer: "Badan Nasional Sertifikasi Profesi (BNSP)",
-    date: "Aug 2024 - Aug 2027",
-    credential: "https://drive.google.com/file/d/1UEAxtFQ5sQQBapcR9lRVq-BT2y0NbEl7/view?usp=drive_link",
-  },
   {
     name: "TOEFL Prediction Test",
     issuer: "Lembaga Bahasa Universitas Bina Sarana Indonesia",
@@ -25,22 +7,10 @@ export const certificates = [
     credential: "https://drive.google.com/file/d/1HA4P_CQA3c1B4kywbU12a8KiCMhDNZ6V/view?usp=drive_link",
   },
   {
-    name: "Network Security",
-    issuer: "Cisco Networking Academy",
-    date: "Jan 2024",
-    credential: "https://lykan.bsi.ac.id/sertifikat_lsp/network/17210529.pdf",
-  },
-  {
-    name: "AWS Academy Graduate — AWS Academy Cloud Foundations",
-    issuer: "Amazon Web Services (AWS)",
-    date: "Feb 2024",
-    credential: "https://www.credly.com/badges/bebc1612-fc72-4fbc-bbee-8aaa305733b1/linked_in_profile",
-  },
-  {
-    name: "AWS Academy Graduate — AWS Academy Cloud Architecting",
-    issuer: "Amazon Web Services (AWS)",
-    date: "Mar 2024",
-    credential: "https://www.credly.com/badges/c1b8558f-9439-41af-8802-211d248924f9/linked_in_profile",
+    name: "ReactJS for Front-end Website Developer",
+    issuer: "Hacktiv8",
+    date: "Dec 2024",
+    credential: "https://drive.google.com/drive/folders/14LQmc7rFcmIRUar1JElmmaiOegjHVROm?usp=sharing",
   },
   {
     name: "Web Development Fundamentals",
@@ -55,10 +25,10 @@ export const certificates = [
     credential: "https://www.linkedin.com/in/handra-putratama-tanjung/details/certifications/1729663108791/single-media-viewer/?profileId=ACoAADfROqcBdgHN9POm8lFkj1bxhQBA6PbhaSE",
   },
   {
-    name: "Pelatihan Pengolahan Pemutakhiran Data Geospasial dan Muatan",
-    issuer: "Badan Pusat Statistik",
-    date: "Apr 2022",
-    credential: "https://drive.google.com/file/d/1deOnGdrgQg1ZIjpOh2AJnQfXsA_4MCnV/view?usp=sharing",
+    name: "Program Analyst",
+    issuer: "Badan Nasional Sertifikasi Profesi (BNSP)",
+    date: "Aug 2024 - Aug 2027",
+    credential: "https://drive.google.com/file/d/1UEAxtFQ5sQQBapcR9lRVq-BT2y0NbEl7/view?usp=drive_link",
   },
   {
     name: "Pelatihan Pembuatan Konten Produk Berbasis Teknologi Tepat Guna dengan Canva untuk KWT Pancasona",
@@ -67,9 +37,39 @@ export const certificates = [
     credential: "https://drive.google.com/file/d/1ddzVFaYc4hPPXh-W7tMdgLuiPKpNb6t7/view?usp=sharing",
   },
   {
-    name: "ReactJS for Front-end Website Developer",
-    issuer: "Hacktiv8",
-    date: "Dec 2024",
-    credential: "https://drive.google.com/drive/folders/14LQmc7rFcmIRUar1JElmmaiOegjHVROm?usp=sharing",
+    name: "Become an AWS Cloud Engineer (Talent Scouting Academy",
+    issuer: "Digital Talent Scholarship",
+    date: "Mar 2024",
+    credential: "https://drive.google.com/file/d/1gzDQfXXCdimIKrbfnDhdhSOuWsB6TjyX/view?usp=drive_link",
+  },
+  {
+    name: "AWS Academy Graduate — AWS Academy Cloud Architecting",
+    issuer: "Amazon Web Services (AWS)",
+    date: "Mar 2024",
+    credential: "https://www.credly.com/badges/c1b8558f-9439-41af-8802-211d248924f9/linked_in_profile",
+  },
+  {
+    name: "AWS Academy Graduate — AWS Academy Cloud Foundations",
+    issuer: "Amazon Web Services (AWS)",
+    date: "Feb 2024",
+    credential: "https://www.credly.com/badges/bebc1612-fc72-4fbc-bbee-8aaa305733b1/linked_in_profile",
+  },
+  {
+    name: "Network Security",
+    issuer: "Cisco Networking Academy",
+    date: "Jan 2024",
+    credential: "https://lykan.bsi.ac.id/sertifikat_lsp/network/17210529.pdf",
+  },
+  {
+    name: "IT BootCamp Software Development",
+    issuer: "Fakultas Teknik & Informatik Universitas Bina Sarana Indonesia",
+    date: "June 2023",
+    credential: "https://says.bsi.ac.id/e_sertifikat_lykan-MzE3MTE1MTExNTU=.html",
+  },
+  {
+    name: "Pelatihan Pengolahan Pemutakhiran Data Geospasial dan Muatan",
+    issuer: "Badan Pusat Statistik",
+    date: "Apr 2022",
+    credential: "https://drive.google.com/file/d/1deOnGdrgQg1ZIjpOh2AJnQfXsA_4MCnV/view?usp=sharing",
   },
 ];
